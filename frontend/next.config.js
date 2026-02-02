d40093eea012f8df4fd9a41611652871d2fdc3d9
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2026-02-02 14:32:03 +0530

    still fixing deployment error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",          // ✅ MUST for Render static site
+//   reactStrictMode: true,
+
+//   images: {
+//     unoptimized: true,       // ✅ static export fix
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",          // ✅ MUST for Render static site
   reactStrictMode: true,
-
-  images: {
-    unoptimized: true,       // ✅ static export fix
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
